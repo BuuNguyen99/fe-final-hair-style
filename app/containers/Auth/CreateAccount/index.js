c7b55c39495bbb60b73ced9c6eeec2fd208d4cf6
Buu <buun@rising-stars.vn>
2022-02-14 23:12:40 +0700

    add validation

--- app/containers/Auth/CreateAccount/index.js
+++ app/containers/Auth/CreateAccount/index.js
@@ -1,14 +1,44 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import clsx from 'clsx';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 export function Registration() {
+  const validationSchema = Yup.object().shape({
+    firstname: Yup.string().required('First Name is required'),
+    lastname: Yup.string().required('Last Name is required'),
+    email: Yup.string()
+      .required('Email is required')
+      .email('Email is invalid'),
+    password: Yup.string()
+      .required('Password is required')
+      .min(6, 'Password must be at least 6 characters')
+      .max(40, 'Password must not exceed 40 characters'),
+    confirmPassword: Yup.string()
+      .required('Confirm Password is required')
+      .oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
+  });
+
+  const onSubmit = data => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
   return (
     <form
       className="form w-100 fv-plugins-bootstrap5 fv-plugins-framework"
       noValidate
       id="kt_login_signup_form"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <div className="mb-5 text-center">
         <h1 className="text-dark mb-3">Create an Account</h1>
@@ -33,30 +63,34 @@ export function Registration() {
 
       <div className="row fv-row">
         <div className="col-xl-6">
-          <label className="form-label fw-bolder text-dark fs-6">
+          <label className="form-label fw-bolder text-dark fs-6 required">
             First name
           </label>
           <input
             placeholder="First name"
+            name="firstname"
             type="text"
+            {...register('firstname')}
+            className={`form-control ${errors.firstname ? 'is-invalid' : ''}`}
             autoComplete="off"
-            className={clsx('form-control form-control-lg form-control-solid')}
           />
+          <div className="invalid-feedback">{errors.firstname?.message}</div>
         </div>
         <div className="col-xl-6">
-          <div className="fv-row mb-5">
-            <label className="form-label fw-bolder text-dark fs-6">
+          <div className="fv-row">
+            <label className="form-label fw-bolder text-dark fs-6 required">
               Last name
             </label>
             <input
               placeholder="Last name"
+              name="lastname"
               type="text"
+              {...register('lastname')}
+              className={`form-control ${errors.lastname ? 'is-invalid' : ''}`}
               autoComplete="off"
-              className={clsx(
-                'form-control form-control-lg form-control-solid',
-              )}
             />
           </div>
+          <div className="invalid-feedback">{errors.lastname?.message}</div>
         </div>
       </div>
 
@@ -68,8 +102,11 @@ export function Registration() {
           placeholder="Email"
           type="email"
           autoComplete="off"
-          className={clsx('form-control form-control-lg form-control-solid')}
+          name="email"
+          {...register('email')}
+          className={`form-control ${errors.email ? 'is-invalid' : ''}`}
         />
+        <div className="invalid-feedback">{errors.email?.message}</div>
       </div>
 
       <div className="mb-4 fv-row" data-kt-password-meter="true">
@@ -78,11 +115,14 @@ export function Registration() {
         </label>
         <div className="position-relative mb-2">
           <input
+            name="password"
             type="password"
             placeholder="Password"
             autoComplete="off"
-            className={clsx('form-control form-control-lg form-control-solid')}
+            {...register('password')}
+            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
           />
+          <div className="invalid-feedback">{errors.password?.message}</div>
         </div>
       </div>
 
@@ -92,10 +132,18 @@ export function Registration() {
         </label>
         <input
           type="password"
+          name="confirmPassword"
           placeholder="Password confirmation"
           autoComplete="off"
-          className={clsx('form-control form-control-lg form-control-solid')}
+          {...register('confirmPassword')}
+          className={`form-control ${
+            errors.confirmPassword ? 'is-invalid' : ''
+          }`}
         />
+
+        <div className="invalid-feedback">
+          {errors.confirmPassword?.message}
+        </div>
       </div>
 
       <div className="text-center">
--- app/containers/Auth/SignIn/index.js
+++ app/containers/Auth/SignIn/index.js
@@ -1,11 +1,40 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import clsx from 'clsx';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 export function Login() {
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .required('Email is required')
+      .email('Email is invalid'),
+    password: Yup.string()
+      .required('Password is required')
+      .min(6, 'Password must be at least 6 characters')
+      .max(40, 'Password must not exceed 40 characters'),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = data => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+
   return (
-    <form className="form w-100" noValidate id="kt_login_signin_form">
+    <form
+      className="form w-100"
+      noValidate
+      id="kt_login_signin_form"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="text-center mb-10">
         <h1 className="text-dark mb-2">Sign In to Hair Salon</h1>
         <div className="text-gray-400 fw-bold fs-4">
@@ -21,22 +50,31 @@ export function Login() {
         </label>
         <input
           placeholder="Email"
-          className={clsx('form-control form-control-lg form-control-solid')}
           type="email"
           name="email"
           autoComplete="off"
+          {...register('email')}
+          className={`form-control form-control-lg form-control-solid ${
+            errors.email ? 'is-invalid' : ''
+          }`}
         />
+        <div className="invalid-feedback">{errors.email?.message}</div>
       </div>
       <div className="fv-row mb-4">
         <label className="form-label fs-6 fw-bolder text-dark required">
           Password
         </label>
         <input
+          name="password"
           type="password"
           placeholder="Password"
           autoComplete="off"
-          className={clsx('form-control form-control-lg form-control-solid')}
+          {...register('password')}
+          className={`form-control form-control-lg form-control-solid ${
+            errors.password ? 'is-invalid' : ''
+          }`}
         />
+        <div className="invalid-feedback">{errors.password?.message}</div>
       </div>
       <div className="fv-row mb-2">
         <Link
